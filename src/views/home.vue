1823e62c89db7a3986c9be7da47821cae47e3ba2
xayrullo <xayrullo.jaloldinov@gmail.com>
2022-08-26 00:38:19 +0500

    metsenat frontend created

---
+++ src/views/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <div class="header">
+      Lorem ipsum dolor sit amet.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  components: {}
+}
+</script>
